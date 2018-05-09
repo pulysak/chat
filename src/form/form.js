0ee2c17999afd9d56807bb14de067690a94750c8
Pavel Lysak <p.lysak@corp.mail.ru>
2018-05-09 23:26:36 +0300

    remove validation

--- src/form/form.js
+++ src/form/form.js
@@ -15,39 +15,8 @@ export class Form {
         `;
     }
 
-    formIsValid(form) {
-        this.formErrors = {};
-        const text = form.querySelector('textarea').value;
-
-        if (!text) {
-            this.formErrors['message-text'] = ['Value is required!'];
-        }
-        return Object.keys(this.formErrors).length === 0;
-    }
-
-    renderErrors() {
-        function insertAfter(elem, refElem) {
-            return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-        }
-
-        for ( const field in this.formErrors ) {
-            this.formErrors[field].forEach(
-                (errorMessage) => {
-                    const errorDiv = document.createElement('div');
-                    errorDiv.innerHTML = errorMessage;
-                    insertAfter(errorDiv, this.el.querySelector(`.${field}`));
-                }
-            );
-        }
-    }
-
-
     _onSubmit(event) {
         event.preventDefault();
-        if (!this.formIsValid(event.target)) {
-            this.renderErrors();
-            return;
-        }
         this.onSubmit({ 'text': event.target.querySelector('textarea').value, 'author': 'test' });
     }
 }
